262c0af2b81174a53e153ea74228882a682f31f9
Wyatt  pefley <wyattpefley@Wyatts-MacBook-Pro.local>
2017-11-21 08:10:23 -0800

    finished the assignment

--- app.js
+++ app.js
@@ -3,18 +3,18 @@
 var allProducts = [];
 var totalClicks = 0;
 var productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
-var imageArray = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
+var files = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.jpg', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
 
-function Product(name, file, clicks, counter) {
-  this.name = name;
-  this.file = file;
-  this.clicks = clicks;
+function Product(productName, imageName, counter, displayCount) {
+  this.productName = productName;
+  this.imageName = imageName;
   this.counter = counter;
+  this.displayCount = displayCount;
   allProducts.push(this);
 }
 
 for(var i = 0; i < productNames.length; i++) {
-  new Product(productNames[i], imageArray[i], 0, 0);
+  new Product(productNames[i], files[i], 0, 0);
 }
 
 var productRank = {
@@ -23,47 +23,60 @@ var productRank = {
   },
 
   displayImages: function() {
-    var indexes = [];
-    while(indexes.length < 3) {
-      console.log('stuck');
+    var index = [];
+    while(index.length < 3) {
       var randomIndex = productRank.getRandomIndex();
-      console.log(randomIndex);
-      console.log(allProducts[randomIndex].clicks);
-      if(allProducts[randomIndex].counter === 0) {
-        allProducts[randomIndex].counter++;
-        indexes.push(randomIndex);
+      if(allProducts[randomIndex].displayCount === 0) {
+        allProducts[randomIndex].displayCount++;
+        index.push(randomIndex);
       }
     }
     for(var i = 0; i < 3; i++) {
-      var oldEl = document.getElementById(i);
-      console.log(oldEl);
-      if(oldEl) {
-        oldEl.remove();
+      var previousImage = document.getElementById(i);
+      if(previousImage) {
+        previousImage.remove();
       }
-      var divEl = document.getElementById('image-' + (i + 1));
-      var imgEl = document.createElement('img');
-      imgEl.src = 'img/' + imageArray[indexes[i]];
-      imgEl.id = i;
-      imgEl.className = [indexes[i]];
-      divEl.appendChild(imgEl);
+      var divElement = document.getElementById('image-' + (i + 1));
+      var imageElement = document.createElement('img');
+      imageElement.src = 'assets/' + files[index[i]];
+      imageElement.id = i;
+      imageElement.className = [index[i]];
+      divElement.appendChild(imageElement);
+    }
+  },
+  tallyClicks: function(elementId) {
+    allProducts[elementId].counter++;
+    totalClicks++;
+    if(totalClicks >= 5) {
+      document.getElementById('image-1').removeEventListener('click', productRank.onClick);
+      document.getElementById('image-2').removeEventListener('click', productRank.onClick);
+      document.getElementById('image-3').removeEventListener('click', productRank.onClick);
+      productRank.displayResults();
     }
   },
 
-  tallyClicks: function(elementId) {
-      // TODO: Hmm... what's going to happen here?
-    },
+  displayResults: function() {
 
-    displayResults: function() {
-      // TODO: Hmm... what's going to happen here?
-    },
+    var divElement = document.getElementById('scores');
+    var pElement = document.createElement('p');
+    pElement.textContent = 'Results';
+    divElement.appendChild(pElement);
+    for(var l = 0; l < allProducts.length; l++) {
+      pElement = document.createElement('p');
+      pElement.textContent = allProducts[l].counter + ' votes for ' + allProducts[l].productName;
+      divElement.appendChild(pElement);
+    }
+  },
 
-    showButton: function() {
-      // TODO: Hmm... what's going to happen here?
-    },
+  showButton: function() {
+  },
 
-    onClick: function() {
-      // TODO: Hmm... what's going to happen here?
-  };
-
-  productRank.imageEls.addEventListener('click', productRank.onClick);
-  productRank.displayImages();
+  onClick: function(event) {
+    productRank.displayImages();
+    productRank.tallyClicks(event.target.className);
+  }
+};
+productRank.displayImages();
+document.getElementById('image-1').addEventListener('click', productRank.onClick);
+document.getElementById('image-2').addEventListener('click', productRank.onClick);
+document.getElementById('image-3').addEventListener('click', productRank.onClick);
