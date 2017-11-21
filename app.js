92f11fa48b08347f6fc32a0984eb0e9c51e2f7a5
Wyatt  pefley <wyattpefley@Wyatts-MacBook-Pro.local>
2017-11-21 07:18:50 -0800

    added some methods

--- app.js
+++ app.js
@@ -1,40 +1,69 @@
+'use strict';
+
 var allProducts = [];
+var totalClicks = 0;
 var productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var imageArray = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
 
-
-function Product(name, path) {
-  // TODO: Build your constructor and necessary properties.
+function Product(name, file, clicks, counter) {
+  this.name = name;
+  this.file = file;
+  this.clicks = clicks;
+  this.counter = counter;
+  allProducts.push(this);
 }
 
-// TODO: Don't forget to build your objects. How can you do this withough having to write 14 lines of `new Product(., ., .)`?
+for(var i = 0; i < productNames.length; i++) {
+  new Product(productNames[i], imageArray[i], 0, 0);
+}
 
 var productRank = {
-  // TODO: All the properties of the object! What do you think you need? Try to write one piece at a time and make sure it does what you want before writing a little more.
-  // NOTE: A-C-P reminder... Make very intentional and iterative changes to your code, and then A-C-P.
-
   getRandomIndex: function() {
-    // TODO: Hmm... what's going to happen here?
+    return Math.floor(Math.random() * allProducts.length);
   },
 
   displayImages: function() {
-    // TODO: Hmm... what's going to happen here?
+    var indexes = [];
+    while(indexes.length < 3) {
+      console.log('stuck');
+      var randomIndex = productRank.getRandomIndex();
+      console.log(randomIndex);
+      console.log(allProducts[randomIndex].clicks);
+      if(allProducts[randomIndex].counter === 0) {
+        allProducts[randomIndex].counter++;
+        indexes.push(randomIndex);
+      }
+    }
+    for(var i = 0; i < 3; i++) {
+      var oldEl = document.getElementById(i);
+      console.log(oldEl);
+      if(oldEl) {
+        oldEl.remove();
+      }
+      var divEl = document.getElementById('image-' + (i + 1));
+      var imgEl = document.createElement('img');
+      imgEl.src = 'img/' + imageArray[indexes[i]];
+      imgEl.id = i;
+      imgEl.className = [indexes[i]];
+      divEl.appendChild(imgEl);
+    }
   },
 
   tallyClicks: function(elementId) {
-    // TODO: Hmm... what's going to happen here?
-  },
+      // TODO: Hmm... what's going to happen here?
+    },
 
-  displayResults: function() {
-    // TODO: Hmm... what's going to happen here?
-  },
+    displayResults: function() {
+      // TODO: Hmm... what's going to happen here?
+    },
 
-  showButton: function() {
-    // TODO: Hmm... what's going to happen here?
-  },
+    showButton: function() {
+      // TODO: Hmm... what's going to happen here?
+    },
 
-  onClick: function() {
-    // TODO: Hmm... what's going to happen here?
-};
+    onClick: function() {
+      // TODO: Hmm... what's going to happen here?
+  };
 
-productRank.imageEls.addEventListener('click', productRank.onClick);
-productRank.displayImages();
+  productRank.imageEls.addEventListener('click', productRank.onClick);
+  productRank.displayImages();
